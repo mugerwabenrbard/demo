d43eec9ee3696020eb825f2ccd95307e1aff46c8
mugerwabenrbard <mugerwabernad96@gmail.com>
2022-11-10 15:15:10 +0300

    make it better

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ app.use('/api/product/', productsRoute)
     // });
     __dirname = path.resolve()
     if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, 'client/build')))
+        app.use(express.static(path.join(__dirname, '/client/build')))
         
-        app.get('*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, '/client', 'build', 'index.html'));
+        app.get('/*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
         });
     }else{
         app.get("/",(req,res)=>{
